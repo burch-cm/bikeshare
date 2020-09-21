681474ef40fc287d076947fa89a5e50346d609e2
Kitt Burch <christopher.m.burch@gmail.com>
2020-09-21 10:47:54 -0400

    data cleaning scripts

--- src/02_convert_to_parquet.bat
+++
@@ -1,9 +0,0 @@
-conda activate parquet
-
-cd %~%\documents\project\csv_to_parquet
-
-python csv_to_parquet_converter -sfp %~%\documents\project\bikeshare\data\tripdata.csv -tfp %~%\documents\project\bikeshare\data\tripdata.parquet
-
-conda deactivate
-
-rm %~%\documents\project\bikeshare\data\tripdata.csv
